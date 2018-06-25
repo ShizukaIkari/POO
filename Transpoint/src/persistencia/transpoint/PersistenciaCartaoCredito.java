252914e8426b6c6f3ecebc3e05a56f25cc2212a8
Serenna <serenna.ferrari@hotmail.com>
2018-06-25 09:45:56 -0300

    minor change
    rly

--- Transpoint/src/persistencia/transpoint/PersistenciaCartaoCredito.java
+++ Transpoint/src/persistencia/transpoint/PersistenciaCartaoCredito.java
@@ -5,10 +5,38 @@
  */
 package persistencia.transpoint;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Serenna
  */
 public class PersistenciaCartaoCredito {
-    
+    private void executeSQL(String sql) {
+        Connection connection = null;
+        Statement stament = null;
+        try {
+            //verifica se as classe da biblioteca existem
+         Class.forName("org.sqlite.JDBC");
+         //abre a conexao com o  banco de dados chamado lanchonete.
+         //esse banco de dado é em arquivo
+         connection  =DriverManager.getConnection("jdbc:sqlite:transpoint.db");
+         System.out.println("Banco de dados aberto");   
+         stament = connection.createStatement();
+         //exeucta o sql no meu banco de dados
+         stament.executeUpdate(sql);
+         stament.close();
+         //fecha a conexao com o banco de dados
+         connection.close();
+         
+            
+        } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+        } catch (SQLException ex) {
+                ex.printStackTrace();
+        }
+    }
 }
