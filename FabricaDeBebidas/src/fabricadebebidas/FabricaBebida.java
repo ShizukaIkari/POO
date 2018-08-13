c6fe07a8267f41a665c091e8f06d964f64588009
Serenna <serenna.ferrari@hotmail.com>
2018-08-13 15:27:59 -0300

    Fábrica de Bebidas
    Exercício de fixação do método fábrica, aplicado a bebidas.

--- FabricaDeBebidas/src/fabricadebebidas/FabricaBebida.java
+++ FabricaDeBebidas/src/fabricadebebidas/FabricaBebida.java
@@ -10,5 +10,23 @@ package fabricadebebidas;
  * @author 20161bsi0284
  */
 public class FabricaBebida {
-    
+    public static Bebida servirBebida(String drink){
+        
+        if (drink.equalsIgnoreCase("cafe")){
+            System.out.println("Café servido!");
+            return new Cafe();
+        } else if (drink.equalsIgnoreCase("refrigerante")){
+            System.out.println("Refrigerante servido!");
+            return new Refrigerante();
+        } else if (drink.equalsIgnoreCase("cha")){
+            System.out.println("Chá servido!");
+            return new Cha();
+        } else if (drink.equalsIgnoreCase("suco")){
+            System.out.println("Suco servido!");
+            return new Suco();
+        } else {
+            System.out.println("Está em falta...");
+        }
+        return null;
+    }
 }
