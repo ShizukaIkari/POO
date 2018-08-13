ad1ccf783312e2a3805bb41b1b3af60aa0f36814
Serenna <serenna.ferrari@hotmail.com>
2018-08-13 11:19:16 -0300

    Exercícios 13/08
    babababababa

---
+++ FabricaLocadora/src/fabricalocadora/InitLocadora.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fabricalocadora;
+
+/* Fábrica da Locadora, responsável por iniciar objetos da locadora  */
+public interface InitLocadora {
+    public Object criarEntidade();
+}
