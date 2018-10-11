062fe018df0949cf7c862a029215469625fa9cb8
Serenna <serenna.ferrari@hotmail.com>
2018-10-11 08:38:03 -0300

    ExercFlyWeight
    Finalizado

--- ExercicioFlyWeight/src/exercicio/FlyweightStatusItem.java
+++ ExercicioFlyWeight/src/exercicio/FlyweightStatusItem.java
@@ -5,10 +5,34 @@
  */
 package exercicio;
 
+import exercicio.StatusItem.Estado;
+import java.util.HashMap;
+
 /**
  *
  * @author Serenna
  */
 public class FlyweightStatusItem {
-    HashMap<> 
+    private static HashMap<Estado,StatusItem>  cache = new HashMap<>();
+    
+    public FlyweightStatusItem (){
+        StatusItem carrinho = new StatusItem(StatusItem.Estado.CARRINHO, true, false);
+        this.addStatus(carrinho);
+        StatusItem compra = new StatusItem(StatusItem.Estado.PAGO, true, false);
+        this.addStatus(compra);
+        StatusItem caminho = new StatusItem(StatusItem.Estado.ENVIADO, true, false);
+        this.addStatus(caminho);
+        StatusItem recebido = new StatusItem(StatusItem.Estado.ENTREGUE, true, false);
+        this.addStatus(recebido);
+        StatusItem closed = new StatusItem(StatusItem.Estado.FECHADO, true, false);
+        this.addStatus(closed);
+    }
+    
+    public void addStatus(StatusItem stats){
+        this.cache.put(stats.getEstado(),stats);
+    }
+    
+    public static StatusItem get(Estado keyEstado){
+        return (StatusItem)cache.get(keyEstado);
+    }
 }
--- ExercicioFlyWeight/src/exercicio/Item.java
+++ ExercicioFlyWeight/src/exercicio/Item.java
@@ -35,7 +35,7 @@ public class Item {
     }
 
     public void setStatus(StatusItem status) {
-        this.status = status;
+        this.status = status;                    
     }
     
     
